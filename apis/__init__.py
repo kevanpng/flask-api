aff03acd2b49962cab016c14446726342c3b3b96
Kevan Png <kevanpngyeowwei@gmail.com>
2019-02-28 01:19:46 +0800

    added blueprints

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,12 +1,10 @@
 from flask_restplus import Api
+from flask import Blueprint
 from .namespace1 import api as ns1
 
 
-api = Api(
-    title='my title',
-    version='1.0',
-    description='random description'
-)
+blueprint = Blueprint('api', __name__)
+api = Api(blueprint)
 
 api.add_namespace(ns1)
 # api.add_namespace(ns1, path='/prefix/of/ns1')
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from apis import api
+from apis import blueprint as api
 
 app = Flask(__name__)
-api.init_app(app)
+app.register_blueprint(api, url_prefix='/api/1')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
